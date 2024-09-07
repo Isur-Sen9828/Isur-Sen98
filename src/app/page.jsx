41ff0f9984b7123df11a1369c343944d56aa806b
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-07 23:24:26 +0530

    created home section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return ( 
+  <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-44">
+    {/* Image container */}
+    <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+      <Image src="/hero.png" alt="" fill className="object-contain"></Image>
+    </div>
+    {/* Text Container */}
+    <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+    {/* Topic */}
+    <h1 className="text-4xl md:text-6xl font-bold ">Crafting  Digital Experiences, Designing Tomorrow. </h1>
+    {/* Description */}
+    <p>Hydration in the context of web development, particularly with React and frameworks like Next.js, 
+      refers to the process of attaching or "hydrating" client-side JavaScript to the static HTML that was 
+      rendered by the server. It enables the static HTML to become interactive on the client side by loading 
+      and running the JavaScript necessary to handle user interactions (like clicking buttons, filling forms, 
+      etc.). </p>
+      {/* Buttons */}
+    <div className="flex gap-4  w-full">
+      <button className="bg-slate-900 p-4 rounded-lg ring-black text-white">View My Work</button>
+      <button className=" p-4 rounded-lg ring-1 ring-black">Contact Me</button>
+    </div>
+    </div>
+
+  </div>
+  )
 };
 
 export default Homepage;
