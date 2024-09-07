fab5367d4fe640ef1ec3d4aa9bc63ccab864503a
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-07 20:52:07 +0530

    completted navBar section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,9 @@
+"use client"
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import NavLink from './navLink';
 
 const links = [
   {url:'/',title:"Home"},
@@ -10,31 +14,65 @@ const links = [
 
 const Navbar = () => {
 
+  const [open, setOpen] = useState(false);
+
+
   return (
-    <div className='h-full flex items-center justify-between px-3 sm:px-8 md:px-12 lg:px-20 xl:px-44'>
+    <div className='h-full flex items-center justify-between px-3 sm:px-8 md:px-12 lg:px-20 xl:px-44 text-xl'>
+      <div className='hidden md:flex gap-4 w-1/3'>
+        {links.map(link=>(
+      <NavLink link={link} key={link.title}/>
+      ))}
+      </div>
       {/* LOGO */}
-      <div className=''>
+      <div className='md:hidden lg:flex xl:w-1/3 xl:justify-center'>
         <Link rel="" href="/" className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'>
         <span className='text-white mr-1 '>Sen</span>
         <span className='w-10 h-8 rounded bg-white text-black flex items-center justify-center'>.dev</span>
         </Link>
       </div>
+      {/* SOCIAL LINKS */}
+      <div className="hidden md:flex gap-3 w-1/3">
+        <Link href="/">
+        <Image src="/github.png"  alt='' width={24} height={24}/>
+        </Link>
+        <Link href="/">
+        <Image src="/linkedin.png"  alt='' width={24} height={24}/>
+        </Link>
+        <Link href="/">
+        <Image src="/pinterest.png"  alt='' width={24} height={24}/>
+        </Link>
+        <Link href="/">
+        <Image src="/dribbble.png"  alt='' width={24} height={24}/>
+        </Link>
+        <Link href="/">
+        <Image src="/facebook.png"  alt='' width={24} height={24}/>
+        </Link>
+        <Link href="/">
+        <Image src="/instagram.png"  alt='' width={24} height={24}/>
+        </Link>
+      </div>
       {/* ResMenu */}
-      <div>
-        <button className=' w-10 h-8 flex flex-col justify-between'>
+      <div className='md:hidden'>
+        <button className=' w-10 h-8 flex flex-col justify-between z-50 relative' 
+        onClick={(()=>setOpen(!open))}> 
+        {/* setOpen((prev) => !prev) also using */}
           <div className='w-10 h-1 bg-black rounded'></div>
           <div className='w-10 h-1 bg-black rounded'></div>
           <div className='w-10 h-1 bg-black rounded'></div>
         </button>
         {/* Menu List */}
-        <div className='absolute top-0 left-0 w-screen '>
+        {open?
+        <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
         {links.map((link)=>(
           <Link href={link.url} key={link.title}>{link.title}</Link>
         ))}
         </div>
+        :''}
       </div> 
     </div>
   )
 }
+
 
 export default Navbar
