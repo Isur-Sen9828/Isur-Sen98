8c9f6341616c37a15595872a19ee4b2a0bce7739
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-09-20 23:54:26 +0530

    feat: created about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,29 @@
+"use client"
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const About = () => {
+  
   return (
-    <div>About</div>
+    <motion.div className="h-full" 
+    initial={{y:"-200vh"}} 
+    animate={{y:"0%"}} 
+    transition={{duration:1}}>
+      {/* Container */}
+      <div>
+        {/* Text container */}
+        <div className='p-4 sm:p-8 md:p-12 lg:p-20 xl:p-40 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-60'>
+          {/* Biograpy container */}
+          <div>BIOGRAPHY</div>
+          {/* Skills container */}
+          <div>SKILLS</div>
+          {/* Experience container */}
+          <div>EXPERIENCE</div>
+        </div>
+        {/* SVG container */}
+        <div className="hidden"></div>
+      </div>
+</motion.div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Homepage = () => {
   return ( 
-    <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+     
   <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-44">
     {/* Image container */}
     <div className="h-1/2 lg:h-full lg:w-1/2 relative">
